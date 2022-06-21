6cc2c1ac3279314aaa593e87d4cc3d489caa1464
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-06-21 19:46:13 +0700

    Production

--- server.js
+++ server.js
@@ -2,38 +2,39 @@ const express = require("express");
 const authRoutes = require("./src/routes/auth");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const serverless = require("serverless-http");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+const database =
+  "mongodb://RohmanM:ex04qnDKfp9k9k95@cluster0-shard-00-00.m7bvb.mongodb.net:27017,cluster0-shard-00-01.m7bvb.mongodb.net:27017,cluster0-shard-00-02.m7bvb.mongodb.net:27017/Users?ssl=true&replicaSet=atlas-12f0mw-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow_Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Header",
-        "Content-Type, Accept, Authorization"
-    );
-    next();
+  res.setHeader("Access-Control-Allow_Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Header",
+    "Content-Type, Accept, Authorization"
+  );
+  next();
 });
 
 app.use(bodyParser.json());
 
-app.use("/v1/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 mongoose
-    .connect(
-        "mongodb://RohmanM:ex04qnDKfp9k9k95@cluster0-shard-00-00.m7bvb.mongodb.net:27017,cluster0-shard-00-01.m7bvb.mongodb.net:27017,cluster0-shard-00-02.m7bvb.mongodb.net:27017/Users?ssl=true&replicaSet=atlas-12f0mw-shard-0&authSource=admin&retryWrites=true&w=majority"
-    )
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`This server running on port : ${port}`);
-            console.log("Connection to DB success");
-        });
-    })
-    .catch((err) => {
-        console.log(err);
-        console.log("Connection to DB failed");
+  .connect(database)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running di ${port}`);
+      console.log("Sukses terhubung ke Mongodb");
     });
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Connection to DB failed");
+  });
