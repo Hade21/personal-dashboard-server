7b915bab214b0f636c00fed0fcb996d5b796e9c2
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-07-08 09:51:52 +0700

    Add update, and delete notes features

--- src/controller/notes.js
+++ src/controller/notes.js
@@ -43,15 +43,97 @@ exports.getAllNotes = (req, res, next) => {
     notesModel
       .find()
       .then((result) => {
+        if (!result.length > 0) {
+          res.status(404).json({
+            message: "Tidak ada catatan!",
+          });
+        }
+
         return res.status(200).json({
           message: "Success",
           data: result,
+          length: result.length,
         });
       })
       .catch((error) => {
-        console.log(error);
+        return res.status(500).json({
+          message: "Failed",
+          data: error,
+        });
       });
   } catch (error) {
     return next(error);
   }
+};
+
+exports.updateNotes = (req, res, next) => {
+  try {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+      const error = new Error("Invalid value!");
+      error.errorStatus = 422;
+      error.data = err;
+      throw error;
+    }
+
+    const { title, body } = req.body;
+    const id = req.params.id;
+
+    notesModel
+      .findById(id)
+      .then((note) => {
+        if (!note) {
+          const error = new Error("Data tidak ditemukan!");
+          error.errorStatus = 404;
+          throw error;
+        }
+
+        note.title = title;
+        note.body = body;
+
+        return note.save();
+      })
+      .then((result) => {
+        return res.status(200).json({
+          message: "Update success!",
+          data: result,
+        });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteNotes = (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    notesModel
+      .findById(id)
+      .then((note) => {
+        if (!note) {
+          const error = new Error("Data tidak ditemukan!");
+          error.errorStatus = 404;
+          throw error;
+        }
+
+        return notesModel
+          .findByIdAndRemove(id)
+          .then((result) => {
+            return res.status(200).json({
+              message: "Success",
+              data: result,
+            });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  } catch (error) {}
 };
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -10,5 +10,7 @@ const { notesValidation } = require("../validator/notes");
 //routing
 router.get("/all", notesController.getAllNotes);
 router.post("/new", notesValidation, notesController.createNotes);
+router.put("/update/:id", notesValidation, notesController.updateNotes);
+router.delete("/delete/:id", notesController.deleteNotes);
 
 module.exports = router;
