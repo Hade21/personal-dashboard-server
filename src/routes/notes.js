8e1fd60f77a7f84776e3a4a23587eab34e8049ba
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-07-09 11:01:42 +0700

    Add authorization to create, update, and delete

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -7,10 +7,18 @@ const notesController = require("../controller/notes");
 //validator
 const { notesValidation } = require("../validator/notes");
 
+//authorization
+const { protect } = require("../middleware/authMiddleware");
+
 //routing
 router.get("/all", notesController.getAllNotes);
-router.post("/new", notesValidation, notesController.createNotes);
-router.put("/update/:id", notesValidation, notesController.updateNotes);
-router.delete("/delete/:id", notesController.deleteNotes);
+router.post("/new", protect, notesValidation, notesController.createNotes);
+router.put(
+  "/update/:id",
+  protect,
+  notesValidation,
+  notesController.updateNotes
+);
+router.delete("/delete/:id", protect, notesController.deleteNotes);
 
 module.exports = router;
